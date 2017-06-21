81e8d5ca56bac189bfbe79f18ab5c9db2755e407
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-06-21 02:56:10 -0700

    Fix folders

---
+++ AppName/app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    BCrypt::Password.new(user.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(supplied_pw)
+    @password = supplied_pw
+    self.password_digest = BCrypt::Password.create(supplied_pw)
+  end
+
+end
